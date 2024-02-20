de18668b8ac618c6a0423a5343edbbed06d11424
andreipu <andrei.purice1203@gmail.com>
2024-02-20 20:21:19 +0200

    first commit

---
+++ src/main/java/com/ApachePDFBox/ApachePdfBoxApplication.java
@@ -0,0 +1,15 @@
+package com.ApachePDFBox;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApachePdfBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApachePdfBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ApachePDFBox/ApachePdfBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApachePDFBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApachePdfBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
